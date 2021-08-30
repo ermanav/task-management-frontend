14a5d2436c4a37f60de9a58adff2e26747fa7e92
ermanav <ermanav@users.noreply.github.com>
2021-08-30 10:51:42 +0000

    deploy: 1af6b98681f83cbb2a58c3c74d429c18a433bbff

---
+++ precache-manifest.5437f573860a44918b9162778f58714c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681212be200c06ae1b63efe53944242e",
+    "url": "https://ermanav.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "64d8c58441863665e031",
+    "url": "https://ermanav.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "7695295970fb2cfe5c81",
+    "url": "https://ermanav.github.io/task-management-frontend/static/js/2.9d422f9e.chunk.js"
+  },
+  {
+    "revision": "64d8c58441863665e031",
+    "url": "https://ermanav.github.io/task-management-frontend/static/js/main.c7e8b037.chunk.js"
+  },
+  {
+    "revision": "386b4ee2246a51778114",
+    "url": "https://ermanav.github.io/task-management-frontend/static/js/runtime~main.8219dfae.js"
+  }
+]);
